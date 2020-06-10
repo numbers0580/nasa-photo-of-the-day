36be50469858a9753846b2eba5b6e0be7a274fca
numbers0580 <koreankid18@yahoo.com>
2020-06-10 15:56:42 -0600

    I got a picture to load, finally.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios"
+import {NASA_URL, MY_API_KEY} from "./constants"
 
 function App() {
+  const [nasaObject, fromNASA] = useState({}); //Found {} use inside useState from stackoverflow.com
+  //useEffect(() => {}, []);
+  useEffect(() => {
+    axios.get(`${NASA_URL}?api_key=${MY_API_KEY}`)
+      .then(nasaData => {
+        //console.log('Inside the NASA success method!'); //Testing ny debugger
+        fromNASA(nasaData.data); //the image is now in nasaData.data.url, which is also nasaObject.url
+        console.log(nasaData);
+        console.log(nasaData.data.url);
+      })
+      .catch(nasaError => {
+        console.log('Error occurred in getting data from NASA.');
+      })
+  }, []);
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        <div>
+          <img src={nasaObject.url}></img>
+        </div>
       </p>
     </div>
   );
