aa18081b9606d78048f903f4c7fab226a80b3efe
numbers0580 <koreankid18@yahoo.com>
2020-06-11 18:22:55 -0600

    Completed React styling of webpage

--- nasa/src/App.js
+++ nasa/src/App.js
@@ -17,13 +17,88 @@ function App() {
         console.log('Error occurred in getting data from NASA.');
       })
   }, []);
-  /*
+
   const myStyles = function() {
     return {
-      //
+      h1: {
+        textAlign: 'center'
+      },
+
+      h2: {
+        margin: '5px'
+      },
+
+      hr: {
+        width: '98%',
+        border: '1px solid black'
+      },
+
+      nav: {
+        width: '98%',
+        margin: '5px',
+        display: 'flex',
+        justifyContent: 'space-between'
+      },
+
+      a: {
+        margin: '5px'
+      },
+
+      section: {
+        margin: '5px 0',
+        width: '100%',
+        height: 'auto',
+        display: 'flex',
+        justifyContent: 'space-around'
+      },
+
+      divLeft: {
+        width: '49%'
+      },
+
+      divRight: {
+        width: '49%',
+        height: '500px',
+        border: '2px solid #000080',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        alignItems: 'flex-start'
+      },
+
+      img: {
+        width: '100%',
+        height: '100%',
+        objectFit: 'cover'
+      },
+
+      p1: {
+        margin: '5px'
+      },
+
+      p2: {
+        margin: '5px',
+        textAlign: 'left'
+      },
+
+      p3: {
+        textAlign: 'center'
+      },
+
+      footer: {
+        position: 'absolute',
+        left: '0',
+        bottom: '0',
+        width: '100%',
+        height: '70px',
+        backgroundColor: '#4D4E4F',
+        color: 'white',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center'
+      }
     }
   }
-  */
 
   return (
     <div className="App">
@@ -42,30 +117,30 @@ function App() {
         </a>
       </header> */}
       <header>
-        <h1 style={{textAlign: 'center'}}>NASA Image of the Day</h1>
-        <nav style={{width: '98%', margin: '5px', display: 'flex', justifyContent: 'space-between'}}>
+        <h1 style={myStyles().h1}>NASA Image of the Day</h1>
+        <nav style={myStyles().nav}>
           <div>
-            <a href="http://www.google.com" style={{margin: '5px'}}>Home</a>
-            <a href="http://www.google.com" style={{margin: '5px'}}>About</a>
+            <a href="http://www.google.com" style={myStyles().a}>Home</a>
+            <a href="http://www.google.com" style={myStyles().a}>About</a>
           </div>
           <div>
-            <a href="http://www.google.com" style={{margin: '5px'}}>Sign-In</a>
+            <a href="http://www.google.com" style={myStyles().a}>Sign-In</a>
           </div>
         </nav>
       </header>
-      <hr style={{width: '98%', border: '1px solid black'}}></hr>
-      <section className="imageContent" style={{margin: '5px 0', width: '100%', height: 'auto', display: 'flex', justifyContent: 'space-around'}}>
-        <div className="imageDiv" style={{width: '49%'}}>
-          <img src={nasaObject.url} alt={nasaObject.title} style={{width: '100%', height: '100%', objectFit: 'cover'}} />
+      <hr style={myStyles().hr}></hr>
+      <section className="imageContent" style={myStyles().section}>
+        <div className="imageDiv" style={myStyles().divLeft}>
+          <img src={nasaObject.url} alt={nasaObject.title} style={myStyles().img} />
         </div>
-        <div class="contentDiv" style={{width: '49%', height: '500px', border: '2px solid #000080', display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'flex-start'}}>
-          <h2 style={{margin: '5px'}}>{nasaObject.title}</h2>
-          <p style={{margin: '5px'}}>Date: {nasaObject.date}</p>
-          <p style={{margin: '5px', textAlign: 'left'}}>{nasaObject.explanation}</p>
+        <div class="contentDiv" style={myStyles().divRight}>
+          <h2 style={myStyles().h2}>{nasaObject.title}</h2>
+          <p style={myStyles().p1}>Date: {nasaObject.date}</p>
+          <p style={myStyles().p2}>{nasaObject.explanation}</p>
         </div>
       </section>
-      <footer style={{position: 'absolute', left: '0', bottom: '0', width: '100%', height: '70px', backgroundColor: '#4D4E4F', color: 'white', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
-        <p style={{textAlign: 'center'}}>Page created by: Peter Wood - June 11, 2020</p>
+      <footer style={myStyles().footer}>
+        <p style={myStyles().p3}>Page created by: Peter Wood - June 11, 2020</p>
       </footer>
     </div>
   );
