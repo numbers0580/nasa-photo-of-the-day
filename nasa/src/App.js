f8869787dbbd91e3b2accde04d39bb8f274458d7
numbers0580 <koreankid18@yahoo.com>
2020-06-11 18:05:18 -0600

    Filled out webpage with NASA API content. Now to convert to React styling

--- nasa/src/App.js
+++ nasa/src/App.js
@@ -1,8 +1,22 @@
 import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import {NASA_URL, MY_API_KEY} from "./constants";
 
 function App() {
+  const [nasaObject, fromNASA] = useState({});
+  useEffect(() => {
+    axios.get(`${NASA_URL}?api_key=${MY_API_KEY}`)
+      .then(nasaData => {
+        fromNASA(nasaData.data); //the image is now in nasaData.data.url, which is also nasaObject.url
+        console.log(nasaData);
+        console.log(nasaData.data.url);
+      })
+      .catch(nasaError => {
+        console.log('Error occurred in getting data from NASA.');
+      })
+  }, []);
   /*
   const myStyles = function() {
     return {
@@ -42,12 +56,12 @@ function App() {
       <hr style={{width: '98%', border: '1px solid black'}}></hr>
       <section className="imageContent" style={{margin: '5px 0', width: '100%', height: 'auto', display: 'flex', justifyContent: 'space-around'}}>
         <div className="imageDiv" style={{width: '49%'}}>
-          <img src="" alt="" style={{width: '100%', height: '100%', objectFit: 'cover'}} />
+          <img src={nasaObject.url} alt={nasaObject.title} style={{width: '100%', height: '100%', objectFit: 'cover'}} />
         </div>
         <div class="contentDiv" style={{width: '49%', height: '500px', border: '2px solid #000080', display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'flex-start'}}>
-          <h2 style={{margin: '5px'}}>2nd Header</h2>
-          <p style={{margin: '5px'}}>Date</p>
-          <p style={{margin: '5px'}}>Explanation</p>
+          <h2 style={{margin: '5px'}}>{nasaObject.title}</h2>
+          <p style={{margin: '5px'}}>Date: {nasaObject.date}</p>
+          <p style={{margin: '5px', textAlign: 'left'}}>{nasaObject.explanation}</p>
         </div>
       </section>
       <footer style={{position: 'absolute', left: '0', bottom: '0', width: '100%', height: '70px', backgroundColor: '#4D4E4F', color: 'white', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
